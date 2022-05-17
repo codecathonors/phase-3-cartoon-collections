9a829562f695450ae98642588966a87015b975e2
CatherineHonors <cathonors@gmail.com>
2022-05-17 10:56:17 -0400

    commit tree be23f2d3548bc94459d1dcf8f96a41a2da6e13ed

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,35 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+
+dwarves = ["Doc", "Dopey", "Bashful", "Grumpy", "Happy", "Sleepy", "Sneezy"]
+
+
+def roll_call_dwarves dwarves
+  dwarves.each_with_index { |dwarf, index| puts "#{index + 1}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+
+def summon_captain_planet planeteer_calls
+  planeteer_calls.map do |call|
+    call.capitalize() + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls planeteer_calls
+  planeteer_calls.any? do |call| 
+    call.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+
+snacks = ["crackers", "gouda", "thyme"]
+
+def find_the_cheese snacks
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  snacks.find do |snack| 
+    cheese_types.include?(snack)
+  end
 end
+
+find_the_cheese(snacks)
